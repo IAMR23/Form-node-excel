5ac6a9b0f810a0ed0c3ec47702f01cd73a52812d
IASP23 <alexanderpilataxi232002@gmail.com>
2025-01-31 22:01:19 -0500

    Cors configuration

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500", // Permitir solo este origen
+    methods: ["GET", "POST"], // Métodos permitidos
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 const archivoExcel = "datos.xlsx";
 
